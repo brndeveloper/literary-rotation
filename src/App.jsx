2350562d5b163bb22e1ab3ae34eb5c0eca53a5dd
Bruno <brunoviniciusfroes@gmail.com>
2024-11-19 11:07:55 -0300

    chore: replace baskervville font with librecaslon

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h1 className="mt-2 bg-red-500 p-6">Rotação Literária!</h1>
+      <h1 className="mt-2 p-6">Rotação Literária!</h1>
     </div>
   );
 }
