c5208bcc264a335b5ee54de5928ac9eaccb442ed
Bruno <brunoviniciusfroes@gmail.com>
2024-12-13 11:39:47 -0300

    style: fix navbar layout for better fitting on smaller devices

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,15 +3,17 @@ import ProfileButton from "./ProfileButton";
 
 const Navbar = () => {
   return (
-    <nav className="flex items-center justify-between px-8 py-5">
-      <div className="flex items-center gap-20 whitespace-nowrap font-medium">
-        <NavItem to="/">Buscar</NavItem>
-        <NavItem to="/meus-livros">Meus Livros</NavItem>
-        <NavItem to="/anunciar">Anunciar</NavItem>
-        <NavItem to="/perfil">Perfil</NavItem>
-      </div>
-      <div className="ml-20">
-        <ProfileButton />
+    <nav className="relative mx-auto ml-2 flex w-full max-w-screen-2xl items-center justify-between px-4 py-4 md:px-8 md:py-5">
+      <div className="flex w-full items-center justify-between">
+        <div className="flex flex-grow flex-wrap items-center gap-8 p-5 sm:gap-20 sm:p-0">
+          <NavItem to="/">Buscar</NavItem>
+          <NavItem to="/meus-livros">Meus livros</NavItem>
+          <NavItem to="/anunciar">Anunciar</NavItem>
+          <NavItem to="/perfil">Perfil</NavItem>
+        </div>
+        <div className="absolute ml-[400px] sm:relative sm:ml-auto">
+          <ProfileButton />
+        </div>
       </div>
     </nav>
   );
