d93a21080c5477f06210cc4d2968d49c2b21ba2e
Bruno <brunoviniciusfroes@gmail.com>
2024-12-13 14:08:49 -0300

    style: improve SendButton style and add responsiveness for small devices

--- src/components/SendButton.jsx
+++ src/components/SendButton.jsx
@@ -1,7 +1,7 @@
 const SendButton = () => {
   return (
     <button
-      className="ml-2 h-[50px] w-[150px] rounded-full border-2 border-[#3c3933] bg-[#6B6059] p-3 text-[#eceae2] shadow-md hover:bg-[#5a524d]"
+      className="ml-2 w-[100px] rounded-full border-2 border-[#3c3933] bg-[#6B6059] p-3 text-[#eceae2] shadow-md transition duration-300 hover:bg-[#5a524d] sm:w-[150px]"
       style={{ height: "50px" }}
     >
       Pesquisar
