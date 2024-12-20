1774466c4822652a4969919fa9bdcf9da83305db
Bruno <brunoviniciusfroes@gmail.com>
2024-12-20 08:48:21 -0300

    feat: add style variants and position options

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,25 @@
 import PropTypes from "prop-types";
 
-const Button = ({ children, ...rest }) => {
+const Button = ({
+  children,
+  variant = "default",
+  position = "left",
+  ...rest
+}) => {
+  const variantClasses = {
+    default: "bg-[#6B6059] hover:bg-[#5a524d] text-[#eceae2]",
+    danger: "bg-[#b52222] hover:bg-[#982525] text-white",
+  };
+
+  const positionClasses = {
+    left: "ml-0 mr-auto",
+    center: "mx-auto",
+    right: "ml-auto mr-0",
+  };
+
   return (
     <button
-      className="mb-2 ml-auto block w-[100px] rounded-full border-2 border-[#3c3933] bg-[#6B6059] p-3 text-[#eceae2] shadow-md transition duration-300 hover:bg-[#5a524d] sm:w-[150px]"
+      className={`mb-2 ${variantClasses[variant]} ${positionClasses[position]} block w-[100px] rounded-full border-2 border-[#3c3933] p-3 shadow-md transition duration-300 sm:w-[150px]`}
       {...rest}
     >
       {children}
@@ -12,6 +28,8 @@ const Button = ({ children, ...rest }) => {
 };
 
 Button.propTypes = {
+  variant: PropTypes.string,
+  position: PropTypes.string,
   children: PropTypes.node.isRequired,
 };
 
